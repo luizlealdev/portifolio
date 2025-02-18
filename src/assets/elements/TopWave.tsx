328a98562370311c8d2868658b414514c36371ea
luizleal <luizandreleal100@gmail.com>
2025-02-18 19:17:41 -0300

    Fix Nextjs build errors

--- src/assets/elements/TopWave.tsx
+++ src/assets/elements/TopWave.tsx
@@ -1,12 +1,6 @@
-const TopWave = ({className}) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-        className={className}
-    >
-        <path
-            d="m0 96 48 26.7c48 26.3 144 80.3 240 112 96 32.3 192 42.3 288 26.6 96-16.3 192-58.3 288-96 96-37.3 192-69.3 288-74.6 96-5.7 192 16.3 240 26.6l48 10.7v192H0z"
-        ></path>
+const TopWave = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" {...props}>
+        <path d="m0 96 48 26.7c48 26.3 144 80.3 240 112 96 32.3 192 42.3 288 26.6 96-16.3 192-58.3 288-96 96-37.3 192-69.3 288-74.6 96-5.7 192 16.3 240 26.6l48 10.7v192H0z"></path>
     </svg>
 );
 
