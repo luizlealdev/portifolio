ad0bc6a0575f302fd3e560f30b467ad131cef839
luizleal <luizandreleal100@gmail.com>
2025-02-18 09:44:50 -0300

    Keep runing animation even when hover is out

--- src/sections/FooterSection.tsx
+++ src/sections/FooterSection.tsx
@@ -1,52 +1,97 @@
-"use client"
+"use client";
 
-import Image from "next/image";
-import Input from "@/components/Input";
-import TextArea from "@/components/TextArea";
-import Button from "@/components/Button";
+import { fadeInLeft, fadeInRight } from "@/animations/scrollAnimations";
 import AndroidEmail from "@/assets/elements/email-android.svg";
-import {InstagramIcon, Mail} from "lucide-react";
-import {Tooltip} from "react-tooltip";
-import {motion} from "framer-motion";
-import {fadeInLeft, fadeInRight} from "@/animations/scrollAnimations";
 import TopWave from "@/assets/elements/TopWave";
 import GithubIcon from "@/assets/icons/GithubIcon";
 import LinkedInIcon from "@/assets/icons/LinkedInIcon";
-import {useSound} from "@/contexts/SoundContext";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import TextArea from "@/components/TextArea";
+import { useSound } from "@/contexts/SoundContext";
+import { motion } from "framer-motion";
+import { InstagramIcon, Mail } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import { Tooltip } from "react-tooltip";
 
 export default function FooterSection() {
-    const {slimeLandSound, slimeDeathSound} = useSound()
+    const [
+        emailIllustrationHoverAnimation,
+        setEmailIllustrationHoverAnimation,
+    ] = useState(false);
+    const [githubLinkButtonAnimation, setGithubButtonAnimation] =
+        useState(false);
+    const [instagramLinkAnimation, setInstagramLinkAnimation] = useState(false);
+    const [linkedinLinkAnimation, setLinkedinLinkAnimation] = useState(false);
+
+    const { slimeLandSound, slimeDeathSound } = useSound();
 
     return (
         <footer>
-            <TopWave className="fill-foreground-primary pointer-events-none select-none"/>
+            <TopWave className="fill-foreground-primary pointer-events-none select-none" />
 
             <div className="bg-foreground-primary pt-6">
                 <div
                     className="max-w-6xl mx-auto px-6 pb-10 flex flex-col-reverse md:flex-1 md:flex-row items-center justify-center md:gap-8 xl:gap-44 scroll-p-80"
-                    id="contact">
-                    <motion.form {...fadeInLeft(20, 0.3, 1)} className="w-full md:w-2/3 lg:w-1/2 xl:w-2/5">
-                        <h3 className="text-4xl text-font-primary font-inter font-bold mb-6">Entre em contato
-                            comigo</h3>
+                    id="contact"
+                >
+                    <motion.form
+                        {...fadeInLeft(20, 0.3, 1)}
+                        className="w-full md:w-2/3 lg:w-1/2 xl:w-2/5"
+                    >
+                        <h3 className="text-4xl text-font-primary font-inter font-bold mb-6">
+                            Entre em contato comigo
+                        </h3>
                         <div className="space-y-4">
-                            <Input label="Nome" placeholder="John Doe" name="name"/>
-                            <Input label="E-mail" name="email" placeholder="johndoe@exemplo.com"
-                                   maxLength={100}/>
-                            <TextArea label="Mensagem" name="message"
-                                      placeholder="Olá, gostaria de falar sobre..." maxLength={300}/>
+                            <Input
+                                label="Nome"
+                                placeholder="John Doe"
+                                name="name"
+                            />
+                            <Input
+                                label="E-mail"
+                                name="email"
+                                placeholder="johndoe@exemplo.com"
+                                maxLength={100}
+                            />
+                            <TextArea
+                                label="Mensagem"
+                                name="message"
+                                placeholder="Olá, gostaria de falar sobre..."
+                                maxLength={300}
+                            />
                             <Button className="w-full">Enviar</Button>
                         </div>
                     </motion.form>
-                    <motion.div {...fadeInRight(20, 0.3, 1)} className="flex md:flex flex-col items-center">
+                    <motion.div
+                        {...fadeInRight(20, 0.3, 1)}
+                        className="flex md:flex flex-col items-center"
+                    >
                         <Image
-                            onMouseEnter={() => slimeDeathSound?.play()}
+                            onMouseEnter={() => {
+                                setEmailIllustrationHoverAnimation(true);
+                                slimeDeathSound?.play();
+                            }}
+                            onAnimationEnd={() =>
+                                setEmailIllustrationHoverAnimation(false)
+                            }
                             src={AndroidEmail}
                             alt="Imagem de envelopes que representam um e-mail com o tema, cores, e o icone do android."
-                            className="w-auto hover:animate-gelatine cursor-pointer"/>
-                        <a data-tooltip-id="open-email-tooltip" data-tooltip-content="Enviar e-mail"
-                           href="mailto:contato@luizleal.dev" target="_blank"
-                           className="hidden md:flex gap-2 transition-all duration-200 hover:text-primary-blue dark:hover:text-font-primary">
-                            <Mail strokeWidth={1.5} className="w-5"/>
+                            className={`w-auto cursor-pointer ${
+                                emailIllustrationHoverAnimation
+                                    ? "animate-gelatine"
+                                    : ""
+                            }`}
+                        />
+                        <a
+                            data-tooltip-id="open-email-tooltip"
+                            data-tooltip-content="Enviar e-mail"
+                            href="mailto:contato@luizleal.dev"
+                            target="_blank"
+                            className="hidden md:flex gap-2 transition-all duration-200 hover:text-primary-blue dark:hover:text-font-primary"
+                        >
+                            <Mail strokeWidth={1.5} className="w-5" />
                             contato@luizleal.dev
                         </a>
 
@@ -56,7 +101,7 @@ export default function FooterSection() {
                                 backgroundColor: "var(--tooltip-bg)",
                                 color: "var(--tooltip-text-color)",
                                 padding: "5px 10px",
-                                borderRadius: "6px"
+                                borderRadius: "6px",
                             }}
                             delayShow={500}
                             place="bottom"
@@ -64,33 +109,75 @@ export default function FooterSection() {
                     </motion.div>
                 </div>
                 <div className="h-px w-full bg-foreground-secondary"></div>
-                <div
-                    className="max-w-6xl mx-auto p-6 flex gap-4 max-[400px]:items-center max-[400px]:flex-col justify-between">
+                <div className="max-w-6xl mx-auto p-6 flex gap-4 max-[400px]:items-center max-[400px]:flex-col justify-between">
                     <p>
                         <span className="font-inter"> &#169; </span>
                         Luiz André Leal · 2025
                     </p>
                     <div className="flex gap-2 fill-font-color">
-                        <a data-tooltip-id="footer-socialmedia-link-tooltip"
-                           data-tooltip-content="Github"
-                           onMouseEnter={() => slimeLandSound?.play()}
-                           href="https://github.com/luizlealdev"
-                           target="_blank">
+                        <a
+                            data-tooltip-id="footer-socialmedia-link-tooltip"
+                            data-tooltip-content="Github"
+                            onMouseEnter={() => {
+                                setGithubButtonAnimation(true);
+                                slimeLandSound?.play();
+                            }}
+                            onAnimationEnd={() =>
+                                setGithubButtonAnimation(false)
+                            }
+                            href="https://github.com/luizlealdev"
+                            target="_blank"
+                        >
                             <GithubIcon
-                                className="hover:animate-gelatine fill-font-color hover:fill-primary-blue dark:hover:fill-font-primary w-6"/>
+                                className={`fill-font-color hover:fill-primary-blue dark:hover:fill-font-primary w-6 ${
+                                    githubLinkButtonAnimation
+                                        ? "animate-gelatine"
+                                        : ""
+                                }`}
+                            />
                         </a>
-                        <a data-tooltip-id="footer-socialmedia-link-tooltip"
-                           data-tooltip-content="Instagram"
-                           onMouseEnter={() => slimeLandSound?.play()}
-                           href="https://github.com/luizlealdev" target="_blank" className="hover:animate-gelatine">
-                            <InstagramIcon className="hover:text-primary-blue dark:hover:text-font-primary w-5"/>
+                        <a
+                            data-tooltip-id="footer-socialmedia-link-tooltip"
+                            data-tooltip-content="Instagram"
+                            onMouseEnter={() => {
+                                setInstagramLinkAnimation(true);
+                                slimeLandSound?.play();
+                            }}
+                            onAnimationEnd={() =>
+                                setInstagramLinkAnimation(false)
+                            }
+                            href="https://github.com/luizlealdev"
+                            target="_blank"
+                            className="hover:animate-gelatine"
+                        >
+                            <InstagramIcon
+                                className={`hover:text-primary-blue dark:hover:text-font-primary w-5 ${
+                                    instagramLinkAnimation
+                                        ? "animate-gelatine"
+                                        : ""
+                                }`}
+                            />
                         </a>
-                        <a data-tooltip-id="footer-socialmedia-link-tooltip"
-                           data-tooltip-content="LinkedIn"
-                           onMouseEnter={() => slimeLandSound?.play()}
-                           href="https://github.com/luizlealdev" target="_blank">
+                        <a
+                            data-tooltip-id="footer-socialmedia-link-tooltip"
+                            data-tooltip-content="LinkedIn"
+                            onMouseEnter={() => {
+                                setLinkedinLinkAnimation(true);
+                                slimeLandSound?.play();
+                            }}
+                            onAnimationEnd={() =>
+                                setLinkedinLinkAnimation(false)
+                            }
+                            href="https://github.com/luizlealdev"
+                            target="_blank"
+                        >
                             <LinkedInIcon
-                                className="hover:animate-gelatine fill-font-color hover:fill-primary-blue dark:hover:fill-font-primary w-6"/>
+                                className={`fill-font-color hover:fill-primary-blue dark:hover:fill-font-primary w-6 ${
+                                    linkedinLinkAnimation
+                                        ? "animate-gelatine"
+                                        : ""
+                                }`}
+                            />
                         </a>
 
                         <Tooltip
@@ -99,13 +186,13 @@ export default function FooterSection() {
                                 backgroundColor: "var(--tooltip-bg)",
                                 color: "var(--tooltip-text-color)",
                                 padding: "5px 10px",
-                                borderRadius: "6px"
+                                borderRadius: "6px",
                             }}
                             place="top"
                         />
-
                     </div>
                 </div>
             </div>
-        </footer>)
+        </footer>
+    );
 }
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -1,73 +1,140 @@
-"use client"
+"use client";
 
-import projects from '@/data/projects.json';
-import {Folder, SquareArrowOutUpRight} from "lucide-react";
+import { fadeInUp } from "@/animations/scrollAnimations";
 import GithubIcon from "@/assets/icons/GithubIcon";
-import {Tooltip} from "react-tooltip";
-import {motion} from "framer-motion";
-import {fadeInUp} from "@/animations/scrollAnimations";
+import { useSound } from "@/contexts/SoundContext";
+import projects from "@/data/projects.json";
+import { motion } from "framer-motion";
+import { Folder, SquareArrowOutUpRight } from "lucide-react";
+import { useState } from "react";
+import { Tooltip } from "react-tooltip";
 
 export default function ProjectsSection() {
-    return (<section className="max-w-6xl mx-auto px-6 pt-16 pb-10 scroll-m-10" id="projects">
-        <motion.div
-            {...fadeInUp(20, 0, 1, "all")}
-            className="mb-14 text-center">
-            <h3
-                className="text-4xl text-font-primary font-inter font-bold">Projetos em destaque
-            </h3>
-            <p className="mt-3">Alguns projetos pessoais e acadêmicos que fiz para praticar</p>
-        </motion.div>
+    return (
+        <section
+            className="max-w-6xl mx-auto px-6 pt-16 pb-10 scroll-m-10"
+            id="projects"
+        >
+            <motion.div
+                {...fadeInUp(20, 0, 1, "all")}
+                className="mb-14 text-center"
+            >
+                <h3 className="text-4xl text-font-primary font-inter font-bold">
+                    Projetos em destaque
+                </h3>
+                <p className="mt-3">
+                    Alguns projetos pessoais e acadêmicos que fiz para praticar
+                </p>
+            </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center gap-6">
-            {projects.map((project, index) => (<ProjectCard key={index} data={project} index={index}/>))}
-        </div>
-    </section>)
-}
-
-const ProjectCard = ({data, index}) => {
-    return (<motion.article
-        {...fadeInUp(50, index * 0.2, 1, "some")}
-        className="bg-background dark:bg-foreground-primary border border-foreground-primary p-6 shadow-md rounded-lg">
-        <div className="flex justify-between">
-                <span className="flex gap-1">
-                    <Folder strokeWidth={1.6} className="w-5 text-font-primary"/>/{data.type.toLowerCase()}
-                </span>
-            <div className="flex gap-3">
-                {data.githubUrl && <a
-                    data-tooltip-id="card-top-buttons-tooltip"
-                    data-tooltip-content="Acessar repositório"
-                    href={data.githubUrl} target="_blank" rel="noopener noreferrer">
-                    <GithubIcon
-                        className="w-6 transition-all duration-200 ease-in-out fill-font-color hover:fill-primary-blue focus:fill-primary-blue dark:hover:fill-font-primary dark:focus:fill-font-primary hover:scale-110"/>
-                </a>}
-                {data.previewUrl && <a data-tooltip-id="card-top-buttons-tooltip"
-                                       data-tooltip-content="Acessar projeto"
-                                       href={data.previewUrl} target="_blank"
-                                       rel="noopener noreferrer">
-                    <SquareArrowOutUpRight strokeWidth={1.7}
-                                           className="w-5 transition-all duration-200 ease-in-out hover:text-primary-blue focus:fill-primary-blue dark:hover:text-font-primary dark:focus:fill-font-primary hover:scale-110"/>
-                </a>}
-
-                <Tooltip
-                    id="card-top-buttons-tooltip"
-                    style={{
-                        backgroundColor: "var(--tooltip-bg)",
-                        color: "var(--tooltip-text-color)",
-                        padding: "5px 10px",
-                        borderRadius: "6px"
-                    }}
-                    delayShow={400}
-                    place="top"
-                />
+            <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center gap-6">
+                {projects.map((project, index) => (
+                    <ProjectCard key={index} data={project} index={index} />
+                ))}
             </div>
-        </div>
-        <div className="mt-4 space-y-1">
-            <h4 className="text-2xl font-inter font-semibold text-font-primary">{data.name}</h4>
-            <p>{data.description}</p>
-        </div>
-        <div className="flex gap-2 flex-wrap text-sm text-primary-blue mt-3">
-            {data.stack.map((stack, index) => (
-                <span className="py-1 px-3 bg-[#4285F4]/10 rounded-full" key={index}>{stack}</span>))}
-        </div>
-    </motion.article>)
+        </section>
+    );
 }
+
+const ProjectCard = ({ data, index }) => {
+    const [githubButtonAnimation, setGithubButtonAnimation] = useState(false);
+    const [previewButtonAnimation, setPreviewButtonAnimation] = useState(false);
+
+    const { slimeLandSound } = useSound();
+
+    return (
+        <motion.article
+            {...fadeInUp(50, index * 0.2, 1, "some")}
+            className="bg-background dark:bg-foreground-primary border border-foreground-primary p-6 shadow-md rounded-lg"
+        >
+            <div className="flex justify-between">
+                <span className="flex gap-1">
+                    <Folder
+                        strokeWidth={1.6}
+                        className="w-5 text-font-primary"
+                    />
+                    /{data.type.toLowerCase()}
+                </span>
+                <div className="flex gap-3">
+                    {data.githubUrl && (
+                        <a
+                            onMouseEnter={() => {
+                                slimeLandSound?.play();
+                                setGithubButtonAnimation(true);
+                            }}
+                            onAnimationEnd={() =>
+                                setGithubButtonAnimation(false)
+                            }
+                            data-tooltip-id="card-top-buttons-tooltip"
+                            data-tooltip-content="Acessar repositório"
+                            href={data.githubUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <GithubIcon
+                                className={`w-6 transition-all duration-200 ease-in-out fill-font-color hover:fill-primary-blue focus:fill-primary-blue dark:hover:fill-font-primary dark:focus:fill-font-primary ${
+                                    githubButtonAnimation
+                                        ? "animate-gelatine"
+                                        : ""
+                                }`}
+                            />
+                        </a>
+                    )}
+                    {data.previewUrl && (
+                        <a
+                            onMouseEnter={() => {
+                                setPreviewButtonAnimation(true);
+                                slimeLandSound?.play();
+                            }}
+                            onAnimationEnd={() =>
+                                setPreviewButtonAnimation(false)
+                            }
+                            data-tooltip-id="card-top-buttons-tooltip"
+                            data-tooltip-content="Acessar projeto"
+                            href={data.previewUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <SquareArrowOutUpRight
+                                strokeWidth={1.7}
+                                className={`w-5 transition-all duration-200 ease-in-out hover:text-primary-blue focus:fill-primary-blue dark:hover:text-font-primary dark:focus:fill-font-primary ${
+                                    previewButtonAnimation
+                                        ? "animate-gelatine"
+                                        : ""
+                                }`}
+                            />
+                        </a>
+                    )}
+
+                    <Tooltip
+                        id="card-top-buttons-tooltip"
+                        style={{
+                            backgroundColor: "var(--tooltip-bg)",
+                            color: "var(--tooltip-text-color)",
+                            padding: "5px 10px",
+                            borderRadius: "6px",
+                        }}
+                        delayShow={400}
+                        place="top"
+                    />
+                </div>
+            </div>
+            <div className="mt-4 space-y-1">
+                <h4 className="text-2xl font-inter font-semibold text-font-primary">
+                    {data.name}
+                </h4>
+                <p>{data.description}</p>
+            </div>
+            <div className="flex gap-2 flex-wrap text-sm text-primary-blue mt-3">
+                {data.stack.map((stack, index) => (
+                    <span
+                        className="py-1 px-3 bg-[#4285F4]/10 rounded-full"
+                        key={index}
+                    >
+                        {stack}
+                    </span>
+                ))}
+            </div>
+        </motion.article>
+    );
+};
