5ee780690ed6b97c4ba8b6285dabfba086eff5f1
luizleal <luizandreleal100@gmail.com>
2025-02-19 14:22:44 -0300

    Add HTML meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type {Metadata} from "next";
-import {Inter, JetBrains_Mono} from "next/font/google";
 import Overlay from "@/components/Overlay";
-import {OverlayProvider} from "@/providers/OverlayProvider";
-import {ThemeModeProvider} from "@/providers/ThemeModeProvider";
+import { OverlayProvider } from "@/providers/OverlayProvider";
+import { ThemeModeProvider } from "@/providers/ThemeModeProvider";
+import type { Metadata } from "next";
+import { Inter, JetBrains_Mono } from "next/font/google";
 
-import "@/styles/globals.css";
+import { SoundProvider } from "@/providers/SoundProvider";
 import "@/styles/animations.css";
-import {SoundProvider} from "@/providers/SoundProvider";
+import "@/styles/globals.css";
 
 const jetBrainsMono = JetBrains_Mono({
     variable: "--font-jetbrains-mono",
@@ -20,30 +20,53 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-    title: "Luiz Leal - Portifolio",
-    description: "Descrição do portifolio",
+    title: "Luiz Leal · Portifólio",
+    description:
+        "Desenvolvedor de apps Android nativo, programo para web quando não tem jeito, e às vezes me aventuro como designer.",
+    applicationName: "Portifólio",
+    authors: [{ name: "Luiz André Leal", url: "https://luizleal.dev" }],
+    creator: "Luiz André Leal",
+    metadataBase: new URL("https://www.luizleal.dev"),
+    openGraph: {
+        title: "Luiz Leal · Portifólio",
+        description:
+            "Desenvolvedor de apps Android nativo, programo para web quando não tem jeito, e às vezes me aventuro como designer.",
+        url: "https://www.luizleal.dev",
+        siteName: "Luiz Leal - Portfólio",
+        type: "website",
+        locale: "pt_BR",
+        images: [
+            {
+                url: "https://www.luizleal.dev/image-preview.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Luiz Leal - Portfólio",
+            },
+        ],
+    },
+    robots: "index, follow",
+    category: "Technology",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="pt-br" suppressHydrationWarning={true}>
-        <body
-            className={`${jetBrainsMono.variable} ${inter.variable} text-font-color antialiased font-jetbrains`}
-        >
-
-        <ThemeModeProvider>
-            <SoundProvider>
-                <OverlayProvider>
-                    {children}
-                    <Overlay/>
-                </OverlayProvider>
-            </SoundProvider>
-        </ThemeModeProvider>
-        </body>
+            <body
+                className={`${jetBrainsMono.variable} ${inter.variable} text-font-color antialiased font-jetbrains`}
+            >
+                <ThemeModeProvider>
+                    <SoundProvider>
+                        <OverlayProvider>
+                            {children}
+                            <Overlay />
+                        </OverlayProvider>
+                    </SoundProvider>
+                </ThemeModeProvider>
+            </body>
         </html>
     );
 }
